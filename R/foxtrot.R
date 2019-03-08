01cfcba1f4b4ec9888d108a769fd66de0578e168
Romain Francois <romain@purrple.cat>
2019-03-08 21:04:48 +0100

    jive() 💃 = bachata() + unnest() + regroup()

--- R/foxtrot.R
+++ R/foxtrot.R
@@ -23,3 +23,23 @@ foxtrot <- function(.tbl, ..., .env = caller_env()) {
 bachata <- function(.tbl, ..., .name = "data", .env = caller_env()) {
   vec_cbind(polka(.tbl), !!.name := foxtrot(.tbl, ..., .env = .env))
 }
+
+#' jive
+#'
+#' @param .tbl TODO
+#' @param ... TODO
+#' @param .env TODO
+#'
+#' @export
+jive <- function(.tbl, ..., .env = caller_env()) {
+  chunks <- foxtrot(.tbl, ..., .env = .env)
+  sizes  <- map_int(chunks, nrow)
+  keys   <- group_keys(.tbl)
+  gps    <- groups(.tbl)
+
+  out <- vec_cbind(
+    keys[rep(seq_len(nrow(keys)), sizes), ],
+    vec_rbind(!!!chunks)
+  )
+  group_by(out, !!!gps)
+}
