1920f962438af8a2b222c9e419519262592ab21f
Romain Francois <romain@purrple.cat>
2019-03-13 09:26:11 +0100

    docs for waltz(), polka(), tango(), charleston() together.
    closes #7
    closes #8
    closes #11
    closes #12
    closes #13

--- R/summarise.R
+++ R/summarise.R
@@ -1,10 +1,51 @@
 
-#' waltz
+#' Summarise one row per group
 #'
-#' @param .tbl TODO
-#' @param ... TODO
-#' @param .env TODO
+#' Applies the [ballet()] and makes sure each results is of size 1,
+#' according to [vctrs::vec_size()]
 #'
+#' @param .tbl A data frame, most likely a grouped data frame
+#' @param ...,.env formulas for each column to create, and parent environment, see [ballet()]
+#' @param .name Name of the packed column made by `charleston()`
+#'
+#' The four functions play a separate role around the idea of
+#' [dplyr::summarise()]:
+#'
+#' - `polka()` peels off one level of grouping from the grouping variable,
+#'    i.e. if `.tbl` was grouped by `x` and `y` the result of `polka()`
+#'    contains columns `x` and `y` and is only grouped by `x`
+#'
+#' - `waltz()` runs the [ballet()] defined `...` and makes
+#'    sure each result is of [vec_size()] 1. The result tibble of `waltz()`
+#'    does not contain the grouping variables.
+#'
+#' - `tango()` is the closest to [dplyr::summarise()], it column binds
+#'   the result of `polka()` and `waltz()` with [vctrs::vec_cbind()].
+#'
+#' - `charleston()` is similar to `tango()` but the results are packed
+#'   instead of being bind. The name of the created package column is
+#'   controled by the `.name` argument.
+#'
+#' @examples
+#' g <- group_by(iris, Species)
+#'
+#' polka(g)
+#'
+#' g %>%
+#'   waltz(Sepal = ~mean(Sepal.Length * Sepal.Width))
+#'
+#' g %>%
+#'   tango(Sepal = ~mean(Sepal.Length * Sepal.Width))
+#'
+#' g %>%
+#'   charleston(Sepal = ~mean(Sepal.Length * Sepal.Width))
+#'
+#' @export
+tango <- function(.tbl, ..., .env = caller_env()) {
+  vec_cbind(polka(.tbl), waltz(.tbl, ..., .env = .env))
+}
+
+#' @rdname tango
 #' @export
 waltz <- function(.tbl, ..., .env = caller_env()) {
   # evaluate all the formulas in each group
@@ -19,9 +60,7 @@ waltz <- function(.tbl, ..., .env = caller_env()) {
   as_tibble_splice(results)
 }
 
-#' polka
-#'
-#' @param .tbl TODO
+#' @rdname tango
 #' @export
 polka <- function(.tbl) {
   groups <- head(groups(.tbl), -1L)
@@ -36,23 +75,7 @@ polka <- function(.tbl) {
   .tbl
 }
 
-#' tango
-#'
-#' @param .tbl TODO
-#' @param ... TODO
-#' @param .env TODO
-#'
-#' @export
-tango <- function(.tbl, ..., .env = caller_env()) {
-  vec_cbind(polka(.tbl), waltz(.tbl, ..., .env = .env))
-}
-
-#' charleston
-#'
-#' @param .tbl TODO
-#' @param ... TODO
-#' @param .name TODO
-#' @param .env TODO
+#' @rdname tango
 #' @export
 charleston <- function(.tbl, ..., .name = "data", .env = caller_env()) {
   vec_cbind(polka(.tbl), tibble(!!.name := waltz(.tbl, ..., .env = .env)))
